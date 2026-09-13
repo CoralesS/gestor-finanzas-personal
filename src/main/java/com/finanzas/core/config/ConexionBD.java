5bc36c552ecf56ac02cf52ca2ce5a90ffcdd36e4
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-12 23:27:13 -0500

    configurar el driver jdbc para la conexión a la BD

--- src/main/java/com/finanzas/core/config/ConexionBD.java
+++ src/main/java/com/finanzas/core/config/ConexionBD.java
@@ -7,18 +7,19 @@ import java.sql.SQLException;
 public class ConexionBD {
 
     // credenciales
-    private static final String URL = "jdbc:mysql://localhost:3306/finanza_personal";
+    private static final String URL = "jdbc:mysql://localhost:3306/name_BD";
     private static final String USER = "root";
-    private static final String PASSWORD = "Coral23";
+    private static final String PASSWORD = "contraseña";
 
     private static Connection conexion = null;
 
     public static Connection getConnection() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //establecer la conexión
             conexion = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Conexion establecida!");
-        } catch (SQLException e){
+        } catch (SQLException | ClassNotFoundException e){
             System.out.println("Error al conectar con la base de datos");
         }
         return conexion;
