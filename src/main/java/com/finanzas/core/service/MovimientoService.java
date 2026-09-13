d7c4b58966a787d308fa902a1f348f3755852835
Dilver-Corales <dilvercorales23@gmail.com>
2026-09-13 00:10:26 -0500

    servicio movimiento completo

--- src/main/java/com/finanzas/core/service/MovimientoService.java
+++ src/main/java/com/finanzas/core/service/MovimientoService.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class MovimientoService {
 
     private MovimientoRepository movRepository;
-
     // Constructor
     public MovimientoService(MovimientoRepository movRepository) {
         this.movRepository = movRepository;
